9ded2c773591b7a21cd57d5bebaace6225daf52a
Thanatermesis <thanatermesis@gmail.com>
2019-05-22 18:03:45 +0200

    fix

--- tree/usr/lib/elive-upgrader/hooks-buster/3.7.1.01/root/fix-permission-temporally.sh
+++ tree/usr/lib/elive-upgrader/hooks-buster/3.7.1.01/root/fix-permission-temporally.sh
@@ -4,10 +4,7 @@ for userdir in /home/*
 do
     if [[ -d "$userdir" ]] ; then
         user="$( basename "$userdir" )"
-        chown "${user}:${user}" "${userdir}/.config/elive-tools/el_config"
-        chown "${user}:${user}" "${userdir}/.config/elive-tools"
         chown "${user}:${user}" "${userdir}/.config/elive"
-        chown "${user}:${user}" "${userdir}/.config"
     fi
 done
 
