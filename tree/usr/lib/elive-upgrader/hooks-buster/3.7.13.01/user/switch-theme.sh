07fc46176d75179df6c22a8d00b3f419e59d8ee4
Thanatermesis <thanatermesis@gmail.com>
2019-10-04 17:15:47 +0200

    add more e16 upgrades

--- tree/usr/lib/elive-upgrader/hooks-buster/3.7.13.01/user/switch-theme.sh
+++ tree/usr/lib/elive-upgrader/hooks-buster/3.7.13.01/user/switch-theme.sh
@@ -3,6 +3,15 @@ source /usr/lib/elive-tools/functions
 
 if [[ -n "$EROOT" ]] ; then
     if [[ -d "/usr/share/e16/themes/Elive_Dark/" ]] ; then
+        rm -f "$HOME/.e16/Init/"*
+        rm -f "$HOME/.e16/New/"*
+        rm -f "$HOME/.e16/Stop/"*
+        cp -a /etc/skel/.e16/Init/* "$HOME/.e16/Init/"
+        cp -a /etc/skel/.e16/New/* "$HOME/.e16/New/"
+        cp -a /etc/skel/.e16/Stop/* "$HOME/.e16/Stop/"
+        sync
+        /usr/share/e16/scripts/e_gen_menu
+        sync
         eesh theme use Elive_Dark
     fi
 fi
