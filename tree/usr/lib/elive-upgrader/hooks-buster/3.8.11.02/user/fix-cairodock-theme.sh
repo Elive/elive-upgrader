0012afa74c841e23adf430e71188b5b46a282dea
Thanatermesis <thanatermesis@gmail.com>
2020-04-24 17:03:07 -0500

    fix for cairo-dock icons

---
+++ tree/usr/lib/elive-upgrader/hooks-buster/3.8.11.02/user/fix-cairodock-theme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source /usr/lib/elive-tools/functions
+
+if pidof cairo-dock 1>/dev/null 2>&1 ; then
+    if ! grep -qs "^default icon directory=gnome" "$HOME/.config/cairo-dock/current_theme/cairo-dock.conf" ; then
+
+        killall cairo-dock
+        sync
+        sed -i -e "s|^default icon directory=.*$|default icon directory=gnome|g" "$HOME/.config/cairo-dock/current_theme/cairo-dock.conf"
+        sed -i -e "s|^default icon directory=.*$|default icon directory=gnome|g" "$HOME/.config/cairo-dock/current_theme/cairo-dock-simple.conf"
+        cairo-dock &
+
+        if [[ -x "$( which notify-send )" ]] ; then
+            el_notify "normal" "logo-elive" "Cairo Dock Icons" "Wohooo! Elive repaired your cairo-dock icons! :)"
+        fi
+    fi
+fi
+
+
