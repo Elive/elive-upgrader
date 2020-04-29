4afb358a44eeeb6188cbfe959f62b5c8d8a5bb9a
Thanatermesis <thanatermesis@gmail.com>
2020-04-28 23:38:33 -0500

    fix

--- tree/usr/lib/elive-upgrader/hooks-buster/3.8.11.04/root/fix.sh
+++ tree/usr/lib/elive-upgrader/hooks-buster/3.8.11.04/root/fix.sh
@@ -3,7 +3,7 @@ SOURCE="$0"
 source /usr/lib/elive-tools/functions
 
 if ! grep -qs "machine-id" /etc/elive-version ; then
-    echo -e "machine-id: $( el_get_machine_id )" >> /etc/elive-version
+    echo -e "machine-id: $( el_get_machine_id | sed -e 's|#machine-id:||g' )" >> /etc/elive-version
 fi
 
 
