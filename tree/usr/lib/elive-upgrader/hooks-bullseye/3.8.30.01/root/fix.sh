041149bc3a5393f154fda8a69c6e9c24d95fefe5
Thanatermesis <thanatermesis@gmail.com>
2022-07-08 20:04:15 -0500

    fix

--- tree/usr/lib/elive-upgrader/hooks-bullseye/3.8.30.01/root/fix.sh
+++ tree/usr/lib/elive-upgrader/hooks-bullseye/3.8.30.01/root/fix.sh
@@ -12,7 +12,7 @@ do
     if [[ -b "$dev" ]] ; then
         if tune2fs -l "$dev" | grep -qsw fast_commit ; then
             el_info "disabling fast_commit in your EXT4 partition '$dev' because can be dangerous for your data"
-            tune2fs -O fast_commit "$dev"
+            tune2fs -O "^fast_commit" "$dev"
         fi
     fi
 done 3<<< "$( cat /proc/mounts | grep "^/dev/" )"
