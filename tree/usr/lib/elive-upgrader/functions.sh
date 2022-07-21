0a031250c902d3d2de43c3daf182b27b411aaf57
Thanatermesis <thanatermesis@gmail.com>
2022-07-21 18:44:12 -0500

    bump

--- tree/usr/lib/elive-upgrader/functions.sh
+++ tree/usr/lib/elive-upgrader/functions.sh
@@ -52,7 +52,7 @@ upgrade_system_delayed(){
     # TODO: change it to monthly
     #limit_time_seconds="2419200" # 27 days (4 weeks - 1 day)  # monthly is the best option for now, to not annoy much the user with popups and updates are not so important, so will have from time to time some yummy improvements (also not much suggestions of desktop upgrades, etc)
     limit_time_seconds="1209600" # 14 days
-    if ((is_buster)) ; then
+    if ((is_bullseye)) ; then
         #limit_time_seconds="518400" # 6 days
         limit_time_seconds="1209600" # 14 days
     fi
