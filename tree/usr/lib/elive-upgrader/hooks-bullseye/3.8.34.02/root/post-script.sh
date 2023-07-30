f9f2b19f16b89eb1140f0256904622d1dd94b342
Thanatermesis <thanatermesis@gmail.com>
2023-07-30 16:00:53 -0500

    post script

--- tree/usr/lib/elive-upgrader/hooks-bullseye/3.8.34.02/root/post-script.sh
+++ tree/usr/lib/elive-upgrader/hooks-bullseye/3.8.34.02/root/post-script.sh
@@ -2,6 +2,7 @@
 
 main(){
     true
+    update-grub
     # sed -i -e 's|^Package: .*$|#&\nPackage: *|g' /etc/apt/preferences.d/backports_priority.pref
     # sed -i -e 's|^Pin-Priority: .*$|#&\nPin-Priority: 500|g' /etc/apt/preferences.d/backports_priority.pref
     #
